76ae7b4427c50fc43b5823a6e4b72e60fe61f754
Пётр Мосин <mosinpetr1@gmail.com>
2024-11-03 18:34:48 +0300

    Begin

---
+++ MasterPol/MasterPol/Pages/ListViewPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using MasterPol.Data;
+
+namespace MasterPol.Pages
+{
+    public partial class ListViewPage : Page
+    {
+        public ListViewPage()
+        {
+            InitializeComponent();
+            MasterListView.ItemsSource = Data.MasterPolEntities.GetContext().Partners_import.ToList();
+        }       
+    }
+}
