8814208145b860cd0ab8b045e44cc544f8347c1a
Мосин Пётр Эдуардович <pmosin@teach.local>
2024-11-05 13:17:07 +0300

    AgainRefresh

--- MasterPol/MasterPol/Data/Model1.Context.cs
+++ MasterPol/MasterPol/Data/Model1.Context.cs
@@ -28,7 +28,7 @@ namespace MasterPol.Data
             }
             return _context;
         }
-    
+
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
             throw new UnintentionalCodeFirstException();
